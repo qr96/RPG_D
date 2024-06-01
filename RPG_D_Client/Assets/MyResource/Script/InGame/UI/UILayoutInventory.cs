306fcf88e8ed7a965b276aef5795797cc3a939e3
qr96 <qr96@naver.com>
2024-06-01 11:50:14 +0900

    commit

--- RPG_D_Client/Assets/MyResource/Script/InGame/UI/UILayoutInventory.cs
+++ RPG_D_Client/Assets/MyResource/Script/InGame/UI/UILayoutInventory.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UILayoutInventory : MonoBehaviour
+public class UILayoutInventory : UILayout
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button dimButton;
+    public GameObject inventoryPopup;
+    public GameObject itemSlotParent;
+    public GameObject itemSlotPrefab;
+
+    public List<GameObject> itemSlotPool = new List<GameObject>();
+
+    private void Awake()
     {
-        
+        dimButton.onClick.AddListener(() =>
+        {
+            ShowPopup(false);
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        itemSlotPrefab.SetActive(false);
+    }
+
+    public void SetInventory(List<int> itemTypes, List<int> itemCounts)
+    {
+        var needSlot = itemCounts.Count - itemSlotPool.Count;
+
+        for (int i = 0; i < needSlot; i++)
+        {
+            var itemSlot = Instantiate(itemSlotPrefab, itemSlotParent.transform);
+            itemSlotPool.Add(itemSlot);
+        }
+
+        foreach (var itemSlot in itemSlotPool)
+            itemSlot.SetActive(false);
+
+        for (int i = 0; i < itemCounts.Count; i++)
+        {
+            var itemSlot = itemSlotPool[i];
+            itemSlot.Find<TMP_Text>("ItemCount").text = itemCounts[i].ToString();
+            itemSlot.SetActive(true);
+        }
+    }
+
+    public void ShowPopup(bool show)
+    {
+        dimButton.gameObject.SetActive(show);
+        inventoryPopup.SetActive(show);
     }
 }
--- RPG_D_Client/Assets/MyResource/Script/InGame/UI/UILayoutMineHUD.cs
+++ RPG_D_Client/Assets/MyResource/Script/InGame/UI/UILayoutMineHUD.cs
@@ -8,19 +8,30 @@ using UnityEngine;
 public class UILayoutMineHUD : UILayout
 {
     public GuageBar hpBar;
-    public List<TMP_Text> itemNotiList;
+    public GameObject ItemNotiBox;
+    public TMP_Text itemNotiPrefab;
 
     Coroutine reduceHpCo;
     Queue<string> itemNotiMessageQue = new Queue<string>();
-    Queue<TMP_Text> itemNotiPool = new Queue<TMP_Text>();
+    List<TMP_Text> itemNotiPool = new List<TMP_Text>();
 
     long maxHp;
     long nowHP;
 
+    const int MAX_ITEM_NOTI = 5;
+
     private void Start()
     {
-        foreach (var item in itemNotiList)
-            itemNotiPool.Enqueue(item);
+        itemNotiPrefab.gameObject.SetActive(false);
+        var itemNotiPivotPos = itemNotiPrefab.GetComponent<RectTransform>().anchoredPosition;
+        var itemNotiHeight = itemNotiPrefab.GetComponent<RectTransform>().rect.height;
+
+        for (int i = 0; i < MAX_ITEM_NOTI; i++)
+        {
+            var itemNoti = Instantiate(itemNotiPrefab, itemNotiPrefab.transform.parent);
+            itemNotiPool.Add(itemNoti);
+            itemNoti.GetComponent<RectTransform>().anchoredPosition = new Vector2(itemNotiPivotPos.x, itemNotiPivotPos.y + i * itemNotiHeight);
+        }
     }
 
     public void SetHpBar(long maxHp, long nowHp)
@@ -60,21 +71,26 @@ public class UILayoutMineHUD : UILayout
 
     void FlushItemMessageQue()
     {
-        if (itemNotiPool.Count > 0)
+        for (int i = 0; i < itemNotiPool.Count; i++)
         {
-            var itemNoti = itemNotiPool.Dequeue();
-            itemNoti.text = itemNotiMessageQue.Dequeue();
-            itemNoti.color = Color.white;
-            itemNoti.gameObject.SetActive(true);
-            itemNoti.DOFade(0f, 0.5f)
-                .SetDelay(1f)
-                .OnComplete(() =>
-                {
-                    itemNoti.gameObject.SetActive(false);
-                    itemNotiPool.Enqueue(itemNoti);
-                    if (itemNotiMessageQue.Count > 0)
+            if (itemNotiMessageQue.Count <= 0)
+                break;
+
+            if (itemNotiPool[i].isActiveAndEnabled == false)
+            {
+                var itemNoti = itemNotiPool[i];
+                itemNoti.text = itemNotiMessageQue.Dequeue();
+                itemNoti.color = Color.white;
+                itemNoti.gameObject.SetActive(true);
+                itemNoti.DOFade(0f, 0.3f)
+                    .SetDelay(1f)
+                    .OnComplete(() =>
+                    {
+                        itemNoti.gameObject.SetActive(false);
                         FlushItemMessageQue();
-                });
+                    });
+                break;
+            }
         }
     }
 }
