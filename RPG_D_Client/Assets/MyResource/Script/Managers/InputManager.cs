7c2d19688bcb740bf7677b691cc194d3e2464226
qr96 <qr96@naver.com>
2024-07-08 00:17:58 +0900

    mod input
    조이스틱 입력값만 normalize 하도록 수정

--- RPG_D_Client/Assets/MyResource/Script/Managers/InputManager.cs
+++ RPG_D_Client/Assets/MyResource/Script/Managers/InputManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class InputManager : MonoBehaviour
 {
@@ -17,6 +16,8 @@ public class InputManager : MonoBehaviour
             inputEvents.Peek().Invoke();
 
         playerInput = Managers.ui.GetLayout<UILayoutJoystick>().GetInput();
+        playerInput = playerInput.normalized;
+
         if (playerInput == Vector2.zero)
         {
             playerInput.x = Input.GetAxis("Horizontal");
